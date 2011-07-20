05b52023fc4792a3b4e13b5de8e62c749830cc8c
Daniel Abrahamsson <hamsson@gmail.com>
2011-07-20 12:10:08 +0200

    Count the number of times a host has been visited

---
+++ stats.js
@@ -0,0 +1,47 @@
+function init() {
+  var clearButton = document.getElementById("clear");
+  clearButton.onclick = function() {
+    localStorage.clear();
+    updateStats();
+  }
+  updateStats();
+}
+
+function sortEntries() {
+  var entries = []
+  for(var host in localStorage) {
+    entries.push(JSON.parse(localStorage[host]));
+  }
+  return entries.sort(function(a, b) {
+    if(b.times > a.times) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+function updateStats() {
+  var statsTable = document.getElementById("stats");
+  
+  while(statsTable.childNodes.length > 0) {
+    statsTable.removeChild(statsTable.firstChild);
+  }
+  
+  // Add new rows
+  var sortedEntries = sortEntries();
+  for(var index in sortedEntries) {
+    entry = sortedEntries[index];
+    var row = document.createElement("tr");
+    var siteCell = document.createElement("td");
+    siteCell.appendChild(document.createTextNode(entry.host));
+    var hoursCell = document.createElement("td");
+    hoursCell.appendChild(document.createTextNode(entry.hours.toFixed(2)));
+    var timesCell = document.createElement("td");
+    timesCell.appendChild(document.createTextNode(entry.times));
+    row.appendChild(siteCell);
+    row.appendChild(hoursCell);
+    row.appendChild(timesCell);
+    statsTable.appendChild(row);
+  }
+}
